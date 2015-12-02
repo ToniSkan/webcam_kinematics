5c63661de5f3eda49e0f47175e80451ff8e662b6
oriolorra <oriolorraserra@gmail.com>
2015-12-02 08:36:29 +0100

    first commit

--- src/urdf_parser.cpp
+++ src/urdf_parser.cpp
@@ -1,13 +1,32 @@
+#include <ros/ros.h>
 #include <kdl_parser/kdl_parser.hpp>
+#include <urdf/model.h>
+#include <iostream>
+ 
+#include <string>
+#include <map>
 
-KDL::Tree my_tree;
-urdf::Model my_model;
+int main(int argc, char *argv[]) 
+{
+	bool exit_value;
+	KDL::Tree my_tree;
+	urdf::Model my_model;
+	
 
-if (!my_model.initXml(....)){
-	ROS_ERROR("Failed to parse urdf robot model");
-	return false;
-}
-if (!kdl_parser::treeFromUrdfModel(my_model, my_tree)){
-	ROS_ERROR("Failed to construct kdl tree");
-	return false;
+	if (argc ==2){
+		if (!my_model.initFile(argv[1])){
+			ROS_ERROR("Failed to parse urdf robot model");
+			return false;
+		}
+		if (!kdl_parser::treeFromUrdfModel(my_model, my_tree)){
+			ROS_ERROR("Failed to construct kdl tree");
+			return false;
+		}
+		std::cout << "Number of links of the urdf:" << my_tree.getNrOfSegments() << std::endl;
+		std::cout << "Number of Joints of the urdf:" << my_tree.getNrOfJoints() << std::endl;
+	
+	}else {
+		ROS_ERROR("Failed to execute, use: rosrun {package} {executable} {path of urdf file}");
+		return false;
+	}
 }
